dd75442f13c67c750f9133cb9834ea4ff87045d1
red4f <60788459+red4f@users.noreply.github.com>
2020-03-05 17:36:04 +0800

    Update r.js

--- r.js
+++ r.js
@@ -3,7 +3,7 @@ for (var i=0, iLen=links.length; i<iLen; i++) {
 var threads = ['https://wwe.lynsr.info/?', 'https://blog.musz.info/?', 'http://faketaxi.s602.xrea.com/go/?'];  
 var random  = threads[Math.floor(Math.random() * threads.length)];
 var hrefer = document.links[i].href;
-if(hrefer.match("wwe.lynsr") || hrefer.match("faketaxi") || hrefer.match("blog.musz")|| hrefer.match(".xyz")|| hrefer.match("ce.alrau")|| hrefer.match("ce.nyrvc")|| hrefer.match("ja.ayrce")) {
+if(hrefer.match("av.alrau") || hrefer.match("anime.musz") || hrefer.match("rf.ayrce")|| hrefer.match(".xyz")|| hrefer.match("ce.alrau")|| hrefer.match("ce.nyrvc")|| hrefer.match("ja.ayrce")) {
 document.links[i].href = document.links[i].href; 
  }else{links[i].href = random + links[i].href;}
  }
