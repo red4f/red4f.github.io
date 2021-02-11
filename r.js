1cca3506022307861a902ded147dcc26716c2c83
red4f <60788459+red4f@users.noreply.github.com>
2021-02-12 02:45:04 +0800

    Update r.js

--- r.js
+++ r.js
@@ -1,6 +1,6 @@
 var links = document.links;
 for (var i=0, iLen=links.length; i<iLen; i++) {
-var threads = ['https://wwe.lynsr.info/?', 'https://blog.musz.info/?', 'http://faketaxi.s602.xrea.com/go/?', 'http://dymusz.s1007.xrea.com/go/?'];  
+var threads = ['https://wwe.lynsr.info/?', 'https://blog.musz.info/?', 'http://faketaxi.s602.xrea.com/go/?', 'http://dymusz.s1007.xrea.com/go/?', 'https://red.lynsr.info/?', 'https://amy.lynsr.info/?'];  
 var random  = threads[Math.floor(Math.random() * threads.length)];
 var hrefer = document.links[i].href;
 if(hrefer.match("av.alrau") || hrefer.match("anime.musz") || hrefer.match("rf.ayrce")|| hrefer.match(".xyz")|| hrefer.match("ce.alrau")|| hrefer.match("ce.nyrvc")|| hrefer.match("ja.ayrce")|| hrefer.match("xx.musz")) {
